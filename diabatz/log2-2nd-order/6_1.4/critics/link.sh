c2255accdd01063cc1f7db6966c192db88b88a95
YifanShenSZ <yshen57@jhu.edu>
2021-06-17 15:40:42 -0400

    try out new fits

---
+++ diabatz/log2-2nd-order/6_1.4/critics/link.sh
@@ -0,0 +1,18 @@
+ln -s ../mass.txt
+ln -s ../opt.IntCoordDef
+
+ln -s ../../IntCoordDef
+ln -s ../../SAS.in
+ln -s ../../min-B1.xyz
+ln -s ../../Hd.in
+ln -s ../../Hd.net
+ln -s ../../11.in
+ln -s ../../12.in
+ln -s ../../13.in
+ln -s ../../14.in
+ln -s ../../22.in
+ln -s ../../23.in
+ln -s ../../24.in
+ln -s ../../33.in
+ln -s ../../34.in
+ln -s ../../44.in
