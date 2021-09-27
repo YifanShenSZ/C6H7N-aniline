daa6905bcbc2b0e627d26e1ab90a9f8c6e5d3ec7
YifanShenSZ <yshen57@jhu.edu>
2021-09-27 08:19:52 -0700

    time to prepare log2-4 and sqrt-4 priors

--- diabatz/log2-4th-order/2_bound_2nd-order/copy_parameters.sh
+++
@@ -1,5 +0,0 @@
-for ((i=1; i<5; i++)); do
-for ((j=i; j<5; j++)); do
-for entry in 'parameters_'$i'-'$j'_'*; do
-    cp 'parameters_'$i'-'$j'_'${entry:15:5} ../3_1.1/'prior_'$i'-'$j'_'${entry:15:5}
-done; done; done
