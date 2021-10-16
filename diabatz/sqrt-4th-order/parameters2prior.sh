96944556dcbf0c510f1411971f57868f1ceb434c
YifanShenSZ <yshen57@jhu.edu>
2021-10-16 17:08:42 -0400

    new diabatz based on G8

--- diabatz/sqrt-4th-order/parameters2prior.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-
-for ((i=1; i<5; i++)); do
-for ((j=i; j<5; j++)); do
-for entry in 'parameters_'$i'-'$j'_'*; do
-    cp 'parameters_'$i'-'$j'_'${entry:15:5} ../5_1.3/'prior_'$i'-'$j'_'${entry:15:5}
-done; done; done;
