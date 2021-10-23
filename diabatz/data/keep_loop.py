7033bb1ffc2d1a5d7568f5b3819835550e1e340f
YifanShenSZ <yshen57@jhu.edu>
2021-10-22 23:33:56 -0400

    new data partition, new input layer

---
+++ diabatz/data/keep_loop.py
@@ -0,0 +1,10 @@
+cart_files = ["geom.data"]
+for i in range(1, 5):
+    cart_files.append("cartgrad-" + str(i) + ".data")
+    for j in range(i + 1, 5):
+        cart_files.append("cartgrad-" + str(i) + "-" + str(j) + ".data")
+for file in cart_files:
+    with open(file, 'r') as f: lines = f.readlines()
+    with open(file, 'w') as f:
+        for line in lines[14 :]:
+            print(line, end='', file=f)
