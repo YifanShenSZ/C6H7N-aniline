c2af4445ac8185d6d794ddfc15ee8a795d50d390
YifanShenSZ <yshen57@jhu.edu>
2021-12-15 10:27:53 -0500

    v0 and v1 scripts

---
+++ diabatz/critics/v1/critics-v1_sad-A2.sh
@@ -0,0 +1,26 @@
+ln -s ../../IntCoordDef
+ln -s ../../SAS.in
+ln -s ../../origin.int
+ln -s ../../Hd.in
+ln -s ../../11.in
+ln -s ../../12.in
+ln -s ../../13.in
+ln -s ../../14.in
+ln -s ../../22.in
+ln -s ../../23.in
+ln -s ../../24.in
+ln -s ../../33.in
+ln -s ../../34.in
+ln -s ../../44.in
+ln -s ../../Hd.net
+
+cp ~/C6H7N-aniline/Columbus/input/IntCoordDef_disso opt.IntCoordDef
+cp ~/C6H7N-aniline/Columbus/sad-A2/sad-A2.xyz .
+ln -s ../mass.txt
+
+~/Software/Mine/diabatz/tools/critics/v1/critics.exe -j sad -f default -i opt.IntCoordDef -t 4 -d default IntCoordDef SAS.in Hd.in Hd.net 11.in 12.in 13.in 14.in 22.in 23.in 24.in 33.in 34.in 44.in -x sad-A2.xyz -c 26 27 28 29 30 31 32 33 34 35 36 -o sad.xyz > critics.log
+
+~/Software/Mine/diabatz/tools/eval/v1/eval.exe -d default IntCoordDef SAS.in Hd.in Hd.net 11.in 12.in 13.in 14.in 22.in 23.in 24.in 33.in 34.in 44.in -x sad.xyz -g    > eval-d.log
+~/Software/Mine/diabatz/tools/eval/v1/eval.exe -d default IntCoordDef SAS.in Hd.in Hd.net 11.in 12.in 13.in 14.in 22.in 23.in 24.in 33.in 34.in 44.in -x sad.xyz -g -a > eval-a.log
+
+~/Software/Mine/diabatz/tools/vibration/v1/vibration.exe -f default -i opt.IntCoordDef -t 4 -x sad.xyz -m mass.txt -d default IntCoordDef SAS.in Hd.in Hd.net 11.in 12.in 13.in 14.in 22.in 23.in 24.in 33.in 34.in 44.in -o sad.log > vibration.log
