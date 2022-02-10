1ffec510708f8868bfae63fbb3a43484f7b31f19
YifanShenSZ <yshen57@jhu.edu>
2022-02-10 13:14:06 -0500

    fit H-N-H with python

---
+++ v1-diabatz/pretrain/H-N-H/python-fit/B1.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+import sklearn.metrics
+import matplotlib.pyplot as plt
+
+# 1. we have bias from ab initio
+# 2. we have pretrained 1st-and-2nd-order weights from bound fit
+def pretraining(x: np.ndarray) -> np.ndarray:
+    y = 0.162201582 \
+      - 1.964265370605905e-06 * x \
+      + 1.085392343776638e-01 * x * x
+    return y
+
+if __name__ == "__main__":
+    data = pd.read_csv("A1B1B2.csv")
+    x = np.array(data["cos(H-N-H)"]) + 0.448660552147955
+    y = np.array(data[" energy B1 / Hartree"]) + 286.1075216397
+    m = x.shape[0]
+
+    y -= pretraining(x)
+
+    order = 8
+    X = np.empty((m, order - 2))
+    X[:, 0] = x * x * x
+    for i in range(1, X.shape[1]):
+        X[:, i] = X[:, i - 1] * x
+
+    XT = X.T
+    Hessian = np.matmul(XT, X)
+    for i in range(Hessian.shape[0]):
+        Hessian[i, i] += 0.0001
+    Hessian_inv = np.linalg.inv(Hessian)
+
+    coeffs = np.matmul(Hessian_inv, np.matmul(XT, y))
+    print("linear regression coefficients are:")
+    for coeff in coeffs:
+        print("%25.15e" % coeff)
+
+    prediction = np.matmul(X, coeffs)
+    print("residue = ", np.linalg.norm(prediction - y))
+    print("R^2 =", sklearn.metrics.r2_score(y, prediction))
+
+    xplot = np.linspace(-1, 1, 100) + 0.448660552147955
+    Xplot = np.empty((xplot.shape[0], order - 2))
+    Xplot[:, 0] = xplot * xplot * xplot
+    for i in range(1, Xplot.shape[1]):
+        Xplot[:, i] = Xplot[:, i - 1] * xplot
+
+    plt.plot(xplot, np.matmul(Xplot, coeffs) + pretraining(xplot))
+    plt.scatter(x, y + pretraining(x))
+    plt.show()
