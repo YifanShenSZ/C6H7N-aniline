bb9a8e6020b0bb09afccb7c57faaee41ef875305
YifanShenSZ <yshen57@jhu.edu>
2022-02-27 00:36:03 -0500

    include C-N-avgH into pretraining

---
+++ v1-diabatz/pretrain/C-N-avgH/python-fit/A1.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+import sklearn.metrics
+import matplotlib.pyplot as plt
+
+# 1. we have bias from ab initio
+# 2. we have pretrained 1st-and-2nd-order weights from bound fit
+def pretraining(x: np.ndarray) -> np.ndarray:
+    y = 0.01300314 \
+      + 8.132494639443186e-02 * x * x
+    return y
+
+if __name__ == "__main__":
+    data = pd.read_csv("energy.csv")
+    x = np.array(data["C-N-avgH"])
+    y = np.array(data[" energy A1 / Hartree"]) + 286.1075216397
+    m = x.shape[0]
+
+    y -= pretraining(x)
+
+    # even orders only
+    order = 6
+    X = np.empty((m, int(order / 2) - 1))
+    X[:, 0] = x * x * x * x
+    for i in range(1, X.shape[1]):
+        X[:, i] = X[:, i - 1] * x * x
+
+    XT = X.T
+    Hessian = np.matmul(XT, X)
+    Hessian_inv = np.linalg.inv(Hessian)
+
+    coeffs = np.matmul(Hessian_inv, np.matmul(XT, y))
+    print("linear regression coefficients are:")
+    for coeff in coeffs:
+        print("%25.15e" % coeff)
+
+    y += pretraining(x)
+    prediction = np.matmul(X, coeffs) + pretraining(x)
+    print("R^2 =", sklearn.metrics.r2_score(y, prediction))
+
+    xplot = np.linspace(-1.0606601717798212, 0.0, 100)
+    Xplot = np.empty((xplot.shape[0], X.shape[1]))
+    Xplot[:, 0] = xplot * xplot * xplot * xplot
+    for i in range(1, Xplot.shape[1]):
+        Xplot[:, i] = Xplot[:, i - 1] * xplot * xplot
+
+    plt.plot(xplot, np.matmul(Xplot, coeffs) + pretraining(xplot))
+    plt.scatter(x, y)
+    plt.show()
