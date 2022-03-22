21fc6da963cc3d0a1a7aa0b08c77882e1cc36136
YifanShenSZ <yshen57@jhu.edu>
2022-03-21 20:17:50 -0400

    include even terms

--- diabatz/bound/script/curve_orientation.py
+++ diabatz/bound/script/curve_orientation.py
@@ -35,17 +35,22 @@ if __name__ == "__main__":
     for i in range(0, len(lines), 2):
         if lines[i].strip() == "bias": break
         coords = line2coords(lines[i])
+        # count unique coordinates and their repeats
         coord_repeats = {}
         for coord in coords:
             if coord not in coord_repeats:
                 coord_repeats[coord] = 1
             else:
                 coord_repeats[coord] += 1
-        # a pure term
-        if len(coord_repeats) == 1:
-            value = list(coord_repeats.values())[0]
-            # an even order term
-            if value % 2 == 0:
-                coeff = float(lines[i + 1])
-                if coeff < 0:
-                    print(lines[i], coeff)
+        # check if all unique coordinates have even repeats
+        orientation_determiner = True
+        for value in coord_repeats.values():
+            if value % 2 != 0:
+                orientation_determiner = False
+                break
+        # output if negative
+        if orientation_determiner:
+            coeff = float(lines[i + 1])
+            if coeff < 0:
+                print(lines[i], coeff)
+
