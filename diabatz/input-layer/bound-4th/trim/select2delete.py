9647a84e5f41f60b7a74187bacd5a19bdceeab83
YifanShenSZ <yshen57@jhu.edu>
2022-04-16 18:01:47 -0400

    organize bound

--- diabatz/input-layer/bound-4th/trim/select2delete.py
+++
@@ -1,7 +0,0 @@
-with open("regset_S_11-max.txt", 'r') as f: lines = f.readlines()
-
-with open("to_delete.txt", 'w') as f:
-    for line in lines:
-        maximum = float(line.split()[1])
-        if maximum < 1e-3:
-            print(line, end='', file=f)
