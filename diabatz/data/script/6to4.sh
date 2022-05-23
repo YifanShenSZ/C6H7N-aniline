60d90f937d05d70c48c9d49e8ea13b0c5fbf302a
YifanShenSZ <yshen57@jhu.edu>
2022-05-23 00:05:11 -0400

    finish new data

---
+++ diabatz/data/script/6to4.sh
@@ -0,0 +1,7 @@
+rm cartgrad-1-4.data cartgrad-2-4.data cartgrad-3-4.data
+rm cartgrad-1-5.data cartgrad-2-5.data cartgrad-3-5.data
+rm cartgrad-4.data cartgrad-5.data cartgrad-4-5.data cartgrad-4-6.data cartgrad-5-6.data
+mv cartgrad-1-6.data cartgrad-1-4.data
+mv cartgrad-2-6.data cartgrad-2-4.data
+mv cartgrad-3-6.data cartgrad-3-4.data
+mv cartgrad-6.data cartgrad-4.data
---
+++ diabatz/data/script/6to4_rm35.sh
@@ -0,0 +1,9 @@
+rm cartgrad-*-3.data cartgrad-3*.data
+rm cartgrad-*-5.data cartgrad-5*.data
+mv cartgrad-1-4.data cartgrad-1-3.data
+mv cartgrad-1-6.data cartgrad-1-4.data
+mv cartgrad-2-4.data cartgrad-2-3.data
+mv cartgrad-2-6.data cartgrad-2-4.data
+mv cartgrad-4-6.data cartgrad-3-4.data
+mv cartgrad-4.data cartgrad-3.data
+mv cartgrad-6.data cartgrad-4.data
