baa87f531ddc542aeb48391e0a2649d6748ef8e2
YifanShenSZ <yshen57@jhu.edu>
2022-05-26 11:38:00 -0400

    reflect Columbus path change

--- diabatz/script/critics-v0/A1rot-sad.sh
+++ diabatz/script/critics-v0/A1rot-sad.sh
@@ -15,7 +15,7 @@ ln -s ../../44.in
 ln -s ../../Hd.net
 
 ln -s ~/C6H7N-aniline/Columbus/input/IntCoordDef_perpendicular opt.IntCoordDef
-ln -s ~/C6H7N-aniline/Columbus/other/NH2-angles/NH2-rotation_from_min-A1/2.2/A1rot-2.2.xyz A1rot-sad.xyz
+ln -s ~/C6H7N-aniline/Columbus/NH2-rotation-A1/2.2/A1rot-2.2.xyz A1rot-sad.xyz
 ln -s ~/C6H7N-aniline/Columbus/input/mass.txt
 
 ~/Software/Mine/diabatz/tools/critics/v0/critics.exe -j min -f default -i opt.IntCoordDef -t 1 -d default IntCoordDef SAS.in Hd.in Hd.net 11.in 12.in 13.in 14.in 22.in 23.in 24.in 33.in 34.in 44.in -x A1rot-sad.xyz -c 25 26 27 28 29 30 31 32 33 34 35 36 -o sad.xyz > critics.log
