5b4071d38e557214f4e892ad340f4c218ec92b7e
YifanShenSZ <yshen57@jhu.edu>
2022-11-22 14:02:19 -0500

    add critics script

---
+++ diabatz/script/critics-v1/critics.sh
@@ -0,0 +1,14 @@
+if [ ! -d critics ]; then
+    mkdir critics
+fi
+cd critics
+
+for geom in min-A1 sad-A1 min-B1 sad-B1 min-B2 sad-B2 min-A2 sad-A2 A1rot-sad B1rot-sad asym; do
+    if [ ! -d $geom ]; then
+        mkdir $geom
+    fi
+    cd $geom
+    bash ~/C6H7N-aniline/diabatz/script/critics-v1/$geom.sh
+done
+
+cd ..
