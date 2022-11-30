3aabbdc1c4c30d80eda0385c442258f168fbf082
YifanShenSZ <yshen57@jhu.edu>
2022-11-29 22:07:50 -0500

    try v1.3.3

--- diabatz/residue/input-layer/README.md
+++ diabatz/residue/input-layer/README.md
@@ -1,2 +1,4 @@
 # Resnet input layer
-simply reuse dissociative input layer
+Irreducible 1: dissociative input layer - 3+ order terms, except the bridged ones
+
+Irreducibles 7, 3, 4: simply reuse dissociative input layer
---
+++ diabatz/residue/train/regularization/pretrain_diag_prior.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+middles = ("1-1", "2-2", "3-3", "4-4")
+
+for middle in middles:
+    with open("pretrain-reg_" + middle + "_1.txt", 'r') as f: reg_lines = f.readlines()
+    with open("../../pretrain/txt2bin/parameters_" + middle + "_1.txt", 'r') as f: par_lines = f.readlines()
+    assert len(reg_lines) == len(par_lines)
+    with open("pretrain-prior_" + middle + "_1.txt", 'w') as f:
+        for i in range(0, len(reg_lines), 2):
+            print(reg_lines[i], end='', file=f)
+            strength = float(reg_lines[i + 1])
+            if strength == 100.0:
+                print("    0.0", file=f)
+            else:
+                print(par_lines[i + 1], end='', file=f)
