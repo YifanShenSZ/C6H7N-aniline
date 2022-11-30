f70a2f278c4f3ea682fb07e2195de0c13b3d8de2
YifanShenSZ <yshen57@jhu.edu>
2022-11-30 15:29:10 -0500

    release 4th order Morse from regularization

--- diabatz/residue/train/regularization/pretrain_diag_prior.py
+++ diabatz/residue/train/regularization/pretrain_diag_prior.py
@@ -3,10 +3,10 @@ from pathlib import Path
 middles = ("1-1", "2-2", "3-3", "4-4")
 
 for middle in middles:
-    with open("pretrain-reg_" + middle + "_1.txt", 'r') as f: reg_lines = f.readlines()
+    with open("pretrain-reg/pretrain-reg_" + middle + "_1.txt", 'r') as f: reg_lines = f.readlines()
     with open("../../pretrain/txt2bin/parameters_" + middle + "_1.txt", 'r') as f: par_lines = f.readlines()
     assert len(reg_lines) == len(par_lines)
-    with open("pretrain-prior_" + middle + "_1.txt", 'w') as f:
+    with open("pretrain-prior/pretrain-prior_" + middle + "_1.txt", 'w') as f:
         for i in range(0, len(reg_lines), 2):
             print(reg_lines[i], end='', file=f)
             strength = float(reg_lines[i + 1])
