56130d361f463ab7c1d4642f8fb8129c27d5dc60
YifanShenSZ <yshen57@jhu.edu>
2022-11-30 20:25:06 -0500

    B1rot-sad was missing in eval

--- diabatz/disso/train/script/eval.sh
+++ diabatz/disso/train/script/eval.sh
@@ -80,7 +80,7 @@ cd ..
 ~/Software/Mine/diabatz/tools/eval/v1/eval.exe \
 -d default pretrain-IntCoordDef pretrain-SAS.in pretrain-Hd.in pretrain-Hd.net pretrain-11.in pretrain-12.in pretrain-13.in pretrain-14.in pretrain-22.in pretrain-23.in pretrain-24.in pretrain-33.in pretrain-34.in pretrain-44.in \
    default IntCoordDef SAS.in Hd.in Hd.net 11.in 12.in 13.in 14.in 22.in 23.in 24.in 33.in 34.in 44.in \
--x ~/C6H7N-aniline/Columbus/NH2-rotation-B1/2.2/B1rot-2.2.xyz > eval/A1rotsad-extend/1.0.log
+-x ~/C6H7N-aniline/Columbus/NH2-rotation-B1/2.2/B1rot-2.2.xyz > eval/B1rotsad-extend/1.0.log
 
 for geom in 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8 2.9 3 4 5 6; do
     ~/Software/Mine/diabatz/tools/eval/v1/eval.exe \
