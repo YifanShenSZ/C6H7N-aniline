5ce8fcc0cbe6232b1693e967b6595f7b77691ac8
YifanShenSZ <yshen57@jhu.edu>
2022-12-24 16:00:13 -0500

    change other loop looking

--- diabatz/analysis/loop/other-loops/plot_single.py
+++ diabatz/analysis/loop/other-loops/plot_single.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 fig, ax = plt.subplots(1, 1)
 
-ax.scatter((1.0, 1.0, 1.4, 1.8), (-40.0, 40.0, 0.0, 0.0), s=1000, marker='x', facecolors="black", linewidth=1.5)
+ax.scatter((1.0, 1.0, 1.4, 1.8), (-40.0, 40.0, 0.0, 0.0), s=1000, marker='x', facecolors="black", linewidth=2)
 
 ax.text(1.0, -55.0, "$^1$B$_1$-$^1$B$_2$", fontsize=24, horizontalalignment="center", verticalalignment="center")
 ax.text(1.0,  25.0, "$^1$B$_1$-$^1$B$_2$", fontsize=24, horizontalalignment="center", verticalalignment="center")
@@ -14,34 +14,34 @@ dx = 0.02
 dy = 2.0
 
 # single 1
-ax.hlines(-20 + dy, 0.9 + dx, 1.1 - dx, colors="blue")
-ax.hlines( 60 - dy, 0.9 + dx, 1.1 - dx, colors="blue")
-ax.vlines(0.9 + dx, -20 + dy,  60 - dy, colors="blue")
-ax.vlines(1.1 - dx, -20 + dy,  60 - dy, colors="blue")
+ax.hlines(-20 + dy, 0.9 + dx, 1.1, colors="black", linewidth=2)
+ax.hlines( 60 - dy, 0.9 + dx, 1.1, colors="black", linewidth=2)
+ax.vlines(0.9 + dx, -20 + dy,  60 - dy, colors="black", linewidth=2)
+ax.vlines(1.1, -20 + dy,  60 - dy, colors="black", linewidth=2)
 
 # single 2
-ax.hlines(-20, 0.9, 1.5, colors="red")
-ax.hlines( 60, 0.9, 1.5, colors="red")
-ax.vlines(0.9, -20,  60, colors="red")
-ax.vlines(1.5, -20,  60, colors="red")
+ax.hlines(-20, 0.9, 1.5, colors="black", linewidth=2)
+ax.hlines( 60, 0.9, 1.5, colors="black", linewidth=2)
+ax.vlines(0.9, -20,  60, colors="black", linewidth=2)
+ax.vlines(1.5, -20,  60, colors="black", linewidth=2)
 
 # single 3
-ax.hlines(-20 - dy, 0.9 - dx, 1.9 + dx, colors="green")
-ax.hlines( 60 + dy, 0.9 - dx, 1.9 + dx, colors="green")
-ax.vlines(0.9 - dx, -20 - dy,  60 + dy, colors="green")
-ax.vlines(1.9 + dx, -20 - dy,  60 + dy, colors="green")
+ax.hlines(-20 - dy, 0.9 - dx, 1.9, colors="black", linewidth=2)
+ax.hlines( 60 + dy, 0.9 - dx, 1.9, colors="black", linewidth=2)
+ax.vlines(0.9 - dx, -20 - dy,  60 + dy, colors="black", linewidth=2)
+ax.vlines(1.9, -20 - dy,  60 + dy, colors="black", linewidth=2)
 
-ax.set_xlabel("N-H / Å"                   , fontsize=24)
-ax.set_ylabel("C1 out of NH$_2$ plane / °", fontsize=24)
+ax.set_xlabel("N-H / Å"                   , fontsize=32)
+ax.set_ylabel("C1 out of NH$_2$ plane / °", fontsize=32)
 
 ax.set_xlim(0.8, 2.0)
 ax.set_ylim(-70, 70)
 
+ax.minorticks_on()
+
 for side in ("top", "bottom", "left", "right"): ax.spines[side].set_linewidth(1.5)
 
-ax.tick_params(direction="in", length=8, width=1.5, labelsize=18)
-
-ax.minorticks_on()
-ax.tick_params(direction="in", length=4, width=1.5, which="minor")
+ax.tick_params(direction="in", length=8, width=2, labelsize=24)
+ax.tick_params(direction="in", length=4, width=2, which="minor")
 
 plt.show()
